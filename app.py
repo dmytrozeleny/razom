01ef8043b333972d92dfe8f3e8db425040c4accb
Dmytro Zelenyi <dmytro.zelenyi@uproad.com>
2023-05-21 18:10:21 +0300

    added style and user data

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, jsonify, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql.expression import func
 
-
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:w5MkULrRIquRYlh@localhost:5432/razom_db"
@@ -20,11 +19,36 @@ class Marker(db.Model):
     route_lon = db.Column(db.ARRAY(db.Numeric(9, 6)), nullable=True)
     start_at = db.Column(db.DateTime(timezone=True), nullable=False)
     short_description = db.Column(db.String(100), nullable=False)
+    created_by = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    participants = db.Column(db.ARRAY(db.Numeric(9, 6)), nullable=True)
+    creator = db.relationship('Users', backref='activities')
+
+class Users(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    phone = db.Column(db.String(50), nullable=False)
 
 def get_route(marker):
     lat_lon_pairs = zip(marker.route_lat, marker.route_lon)
     route = [[float(lat), float(lon)] for lat, lon in lat_lon_pairs]
     return route
+
+def generate_output(markers):
+    return jsonify({'markers': [{'id' : marker.id,
+                                 'title': marker.activity_type,
+                                 'lat': marker.start_lat,
+                                 'lon': marker.start_lon,
+                                 'short_description':marker.short_description,
+                                 'start_at':marker.start_at,
+                                 'estimated_duration_minutes':marker.estimated_duration_minutes,
+                                 'created_by':marker.creator.first_name + ' ' + marker.creator.last_name,
+                                 'details_url': url_for('marker_details', marker_id=marker.id)
+                                 } for marker in markers]})
+
 
 @app.route('/')
 def index():
@@ -37,36 +61,28 @@ def get_markers():
         if boundaries:
             sw_lat, sw_lon = boundaries['sw_lat'], boundaries['sw_lon']
             ne_lat, ne_lon = boundaries['ne_lat'], boundaries['ne_lon']
-            markers = Marker.query.filter(Marker.start_lat >= sw_lat, Marker.start_lat <= ne_lat,
+            markers = Marker.query.join(Users, Marker.created_by == Users.id).filter(Marker.start_lat >= sw_lat, Marker.start_lat <= ne_lat,
                                           Marker.start_lon >= sw_lon, Marker.start_lon <= ne_lon).order_by(func.random()).limit(10).all()
         else:
-            markers = Marker.query.order_by(func.random()).limit(10).all()
-        return jsonify({'markers': [{'id' : marker.id,
-                                     'title': marker.activity_type,
-                                     'lat': marker.start_lat,
-                                     'lon': marker.start_lon,
-                                     'short_description':marker.short_description,
-                                     'start_at':marker.start_at,
-                                     'estimated_duration_minutes':marker.estimated_duration_minutes,
-                                     'details_url': url_for('marker_details', marker_id=marker.id)
-                                     } for marker in markers]})
+            markers = Marker.query.join(Users, Marker.created_by == Users.id).order_by(func.random()).limit(10).all()
+        print([m for m in markers])
+        return generate_output(markers)
     else:
-        markers = Marker.query.order_by(func.random()).limit(10).all()
-        return jsonify({'markers': [{'id' : marker.id,
-                                     'title': marker.activity_type,
-                                     'lat': marker.start_lat,
-                                     'lon': marker.start_lon,
-                                     'short_description':marker.short_description,
-                                     'start_at':marker.start_at,
-                                     'estimated_duration_minutes':marker.estimated_duration_minutes,
-                                     'details_url': url_for('marker_details', marker_id=marker.id)
-                                     } for marker in markers]})
-    
+        markers = Marker.query.join(Users, Marker.created_by == Users.id).order_by(func.random()).limit(10).all()
+        return generate_output(markers)
+
 @app.route('/marker_details/<int:marker_id>')
 def marker_details(marker_id):
     # Get Marker data from the database using marker_id
     marker = Marker.query.filter_by(id=marker_id).first()
-
+    participants_ids = marker.participants
+    participants = Users.query.filter(Users.id.in_(participants_ids)).all()
+    participant_names = [p.first_name + ' ' + p.last_name for p in participants]
+    participant_phones = [p.phone for p in participants]
+    participant_emails = [p.email for p in participants]
+    participants = {
+        'name':participant_names
+    }
     # Get route from Marker data
     route = get_route(marker)
     print(route)
@@ -78,7 +94,11 @@ def marker_details(marker_id):
                            route=route,
                            short_description=marker.short_description,
                            start_time=marker.start_at.strftime('%Y-%m-%d %H:%M:%S'),
-                           estimated_duration=marker.estimated_duration_minutes)
+                           estimated_duration=marker.estimated_duration_minutes,
+                           participant_names=participant_names,
+                           participant_phones=participant_phones,
+                           participant_emails=participant_emails,
+                           )
 
 
 if __name__ == '__main__':
