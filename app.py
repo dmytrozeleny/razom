2df8e3fa882c46ad1b9e983c7a93c357e0c1f96e
Dmytro Zelenyi <dmytro.zelenyi@uproad.com>
2023-06-04 20:57:36 +0300

    added complex zoom

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request, url_for
+from flask import Flask, render_template, jsonify, request, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql.expression import func
 import os
@@ -7,7 +7,8 @@ database_url = os.getenv('DATABASE_URL')
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = database_url.replace('postgres://', 'postgresql://', 1)
+app.secret_key = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:w5MkULrRIquRYlh@localhost:5432/razom_db"
 db = SQLAlchemy(app)
 
 class Marker(db.Model):
@@ -41,7 +42,19 @@ def get_route(marker):
     return route
 
 def generate_output(markers):
-    return jsonify({'markers': [{'id' : marker.id,
+    return jsonify({'markers': [{'id' : marker['id'],
+                                 'title': marker['title'],
+                                 'lat': marker['lat'],
+                                 'lon': marker['lon'],
+                                 'short_description':marker['short_description'],
+                                 'start_at':marker['start_at'],
+                                 'estimated_duration_minutes':marker['estimated_duration_minutes'],
+                                 'created_by':marker['created_by'],
+                                 'details_url': 'details_url'
+                                 } for marker in markers]})
+
+def markers_to_dict(markers):
+    return [{'id' : marker.id,
                                  'title': marker.activity_type,
                                  'lat': marker.start_lat,
                                  'lon': marker.start_lon,
@@ -50,29 +63,90 @@ def generate_output(markers):
                                  'estimated_duration_minutes':marker.estimated_duration_minutes,
                                  'created_by':marker.creator.first_name + ' ' + marker.creator.last_name,
                                  'details_url': url_for('marker_details', marker_id=marker.id)
-                                 } for marker in markers]})
+                                 } for marker in markers]
 
 
 @app.route('/')
 def index():
+    if 'previously_loaded_markers' in session:  
+        session.pop('previously_loaded_markers',None)
+    if 'previous_zoom' in session:
+        session.pop('previous_zoom',None)
     return render_template('index.html')
 
 @app.route('/markers', methods=['GET', 'POST'])
 def get_markers():
     if request.method == 'POST':
-        boundaries = request.get_json()
-        if boundaries:
-            sw_lat, sw_lon = boundaries['sw_lat'], boundaries['sw_lon']
-            ne_lat, ne_lon = boundaries['ne_lat'], boundaries['ne_lon']
-            markers = Marker.query.join(Users, Marker.created_by == Users.id).filter(Marker.start_lat >= sw_lat, Marker.start_lat <= ne_lat,
-                                          Marker.start_lon >= sw_lon, Marker.start_lon <= ne_lon).order_by(func.random()).limit(10).all()
+        if 'previously_loaded_markers' not in session:
+            print('Initial launch, no previously loaded markers')
+            boundaries = request.get_json()
+            if boundaries:
+                print(boundaries)
+                sw_lat, sw_lon = boundaries['sw_lat'], boundaries['sw_lon']
+                ne_lat, ne_lon = boundaries['ne_lat'], boundaries['ne_lon']
+                zoom = boundaries['zoom']
+                markers = Marker.query.join(Users, Marker.created_by == Users.id).filter(Marker.start_lat >= sw_lat, Marker.start_lat <= ne_lat,
+                                            Marker.start_lon >= sw_lon, Marker.start_lon <= ne_lon).order_by(func.random()).limit(10).all()
+            else:
+                markers = Marker.query.join(Users, Marker.created_by == Users.id).order_by(func.random()).limit(10).all()
+            print('    recording markers and zoom into session')
+            session['previously_loaded_markers'] = markers_to_dict(markers)
+            session['previous_zoom'] = zoom
+            print('=======================')
+            markers = session['previously_loaded_markers']
         else:
-            markers = Marker.query.join(Users, Marker.created_by == Users.id).order_by(func.random()).limit(10).all()
-        print([m for m in markers])
-        return generate_output(markers)
+            print('Previously loaded markers found:', session['previously_loaded_markers'])
+            print('Previous zoom found:', session['previous_zoom'])
+
+            boundaries = request.get_json()
+            if boundaries:
+                print(boundaries)
+                sw_lat, sw_lon = boundaries['sw_lat'], boundaries['sw_lon']
+                ne_lat, ne_lon = boundaries['ne_lat'], boundaries['ne_lon']
+                zoom = boundaries['zoom']
+                if zoom >= session['previous_zoom']:
+                    print('    Zoom in or same zoom')
+                    bad_markers = [m for m in session['previously_loaded_markers']
+                                   if not (float(m['lat']) >= sw_lat and float(m['lat']) <= ne_lat
+                                        and float(m['lon']) >= sw_lon and float(m['lon']) <= ne_lon)
+                                        ]
+                    print('    bad markers', bad_markers)
+                    bad_markers_ids = [m['id'] for m in bad_markers]
+                    bad_markers_len = len(bad_markers)
+                    print('bad markers len:', bad_markers_len)
+                    if bad_markers_len > 0:
+                        markers = Marker.query.join(Users, Marker.created_by == Users.id).filter(Marker.start_lat >= sw_lat, Marker.start_lat <= ne_lat,
+                                                    Marker.start_lon >= sw_lon, Marker.start_lon <= ne_lon, Marker.id.notin_(bad_markers_ids)).order_by(func.random()).limit(bad_markers_len).all()
+                        print('        newly queried markers', markers)
+                        if len(markers) > 0:
+                            session['previously_loaded_markers'] = [m for m in session['previously_loaded_markers'] if m not in bad_markers]
+                            session['previously_loaded_markers'] = session['previously_loaded_markers'] + markers_to_dict(markers)
+                            print('    Updating prev updated', session['previously_loaded_markers'])
+                            session['previous_zoom'] = zoom
+                            markers = session['previously_loaded_markers']
+                        else:
+                            session['previous_zoom'] = zoom
+                            markers = session['previously_loaded_markers']
+                    else:
+                        session['previous_zoom'] = zoom
+                        markers = session['previously_loaded_markers']
+                else:
+                    print('    Zoom out')
+                    markers = Marker.query.join(Users, Marker.created_by == Users.id).filter(Marker.start_lat >= sw_lat, Marker.start_lat <= ne_lat,
+                                                Marker.start_lon >= sw_lon, Marker.start_lon <= ne_lon).order_by(func.random()).limit(7).all()
+                    print('Markers len', len(markers))
+                    if len(markers) > 0:
+                        session['previously_loaded_markers'] = session['previously_loaded_markers'][:5]
+                        session['previously_loaded_markers'] = session['previously_loaded_markers'] + markers_to_dict(markers)
+                        print('        newly queried markers', markers)
+                        session['previous_zoom'] = zoom
+                        markers = session['previously_loaded_markers']
+                    else:
+                        session['previous_zoom'] = zoom
+                        markers = session['previously_loaded_markers']
     else:
         markers = Marker.query.join(Users, Marker.created_by == Users.id).order_by(func.random()).limit(10).all()
-        return generate_output(markers)
+    return generate_output(markers)
 
 @app.route('/marker_details/<int:marker_id>')
 def marker_details(marker_id):
@@ -102,7 +176,6 @@ def marker_details(marker_id):
                            participant_phones=participant_phones,
                            participant_emails=participant_emails,
                            )
-
 
 if __name__ == "__main__":
     from waitress import serve
